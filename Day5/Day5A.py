e1eea37484998f5ce5a6903b3098e1bac1fc65bd
jeff <jeff@schmitzes.com>
2020-05-28 22:18:44 -0400

    Day 5 cont

--- Day5/Day5A.py
+++ Day5/Day5A.py
@@ -4,22 +4,20 @@
 # if does NOT contain ab, cd, pq, or xy
 # else it is naughty
 
-from collections import Counter
 naughty = 0
 nice = 0
 n_strings = ["ab", "cd", "pq", "xy"]
-vowel_string = ["a", "e", "i", "o", "u"]
-vowel_count = 0
+vowels = 'aeiou'
+input = open('input').read().split('\n')
 
 def vowel_counter():
-    for i in open('input').readlines():
-        
+    if i in 'aeiou':
 
-for i in open('input').readlines():
-    if any(x in i for x in n_strings):
-        continue
-        # naughty += 1
-        # print(i)
-    elif vowel_counter() >= 3:
-        nice += 1
 
+
+for i in input:
+    if not (any(x in i for x in n_strings)):
+        if vowel_counter() >= 3:
+            nice += 1
+
+#print(nice)
